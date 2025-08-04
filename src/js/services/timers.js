71aab4d66967869964f3edcc8ec5eadb016fd476
Oliwan13 <olivio.jaonina@gmail.com>
2025-08-04 21:31:28 +0200

    Create timers.js

---
+++ src/js/services/timers.js
@@ -0,0 +1,17 @@
+/**
+ * Manages individual rest timers for each exercise.
+ */
+import { state } from '../state.js';
+import { formatTimerDisplay } from '../utils/formatters.js';
+
+function playBeep() {
+    // Beep implementation...
+}
+
+export function handleTimerActions(timerAction, idx, element) {
+    // Timer logic from original file...
+}
+
+export function loadPersistentIndividualTimers() {
+    // Logic to load timers from localStorage on page load...
+}
