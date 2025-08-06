0299c3f7232d4ce87edd63fe3cfad708f555ca5a
Oliwan13 <olivio.jaonina@gmail.com>
2025-08-06 13:36:47 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,16 @@ import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // No special configuration needed for this vanilla setup
+  // Project root directory (where index.html is)
+  root: 'src',
+  // Base public path when served in development or production
+  base: './',
+  // Directory to serve as plain static assets
+  publicDir: '../public',
+  build: {
+    // Directory to output build files
+    outDir: '../dist',
+    // Empty the output directory before building
+    emptyOutDir: true,
+  },
 });
